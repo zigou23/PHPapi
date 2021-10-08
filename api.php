ae2a717a36403ad29c56c6446cab99cfb8b176a8
zigouisim <40587032+zigou23@users.noreply.github.com>
2021-10-08 14:42:28 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,36 +1,16 @@
-<?php
-// 存储数据的文件
-$filename = 'data.dat';        
- 
-// 指定页面编码
-header('Content-type: text/html; charset=utf-8');
- 
-if(!file_exists($filename)) {
-    die($filename . ' 数据文件不存在');
+<?php 
+//获取句子文件的绝对路径
+$path = dirname(__FILE__);
+$file = file($path."/hitokoto.txt");
+
+//随机读取一行
+$arr  = mt_rand( 0, count( $file ) - 1 );
+$content  = trim($file[$arr]);
+
+//格式化判断，输出js或纯文本
+if ($_GET['encode'] === 'js') {
+    echo "function hitokoto(){document.write('" . $content ."');}";
+} else {
+    echo $content;
 }
- 
-$data = array();
- 
-// 打开文档
-$fh = fopen($filename, 'r');
- 
-// 逐行读取并存入数组中
-while (!feof($fh)) {
-    $data[] = fgets($fh);
-}
- 
-// 关闭文档
-fclose($fh);
- 
-// 随机获取一行索引
-$result = $data[array_rand($data)];
-
-//直接输出结果
-echo $result; 
-
-//网页直接js调用，参考 https://www.17kafei.net/archives/14/ 
-//方式 <script src="http://你的网址/api.php"></script>
-//echo 'document.write("'.htmlspecialchars($result).'");';
-
-
-//echo '(function zigouapi(){var zigouapi="'.htmlspecialchars($result).'";var dom=document.querySelector("#zigouapi");Array.isArray(dom)?dom[0].innerText=zigouapi:dom.innerText=zigouapi;})()'
+?>
