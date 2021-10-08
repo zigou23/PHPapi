f2e3ef63172c0d305e1af16dd5c4eb2a1c8b3ab3
zigouisim <40587032+zigou23@users.noreply.github.com>
2021-10-08 15:08:23 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,13 +1,28 @@
-<?php 
-//获取句子文件的绝对路径
-$path = dirname(__FILE__);
-$file = file($path."/hitokoto.txt");
+<?php
+header("Content-type: text/html; charset=utf-8"); 
 
-//随机读取一行
+$path = dirname(__FILE__);
+ 
+if (isset($_GET['type']) && !empty($_GET['type'])) {
+    $type = $_GET['type'];
+    $file = file($path."/".$type.".txt");
+} else {
+    $file = file($path."/hitokoto.txt");
+}
+ 
+ 
 $arr  = mt_rand( 0, count( $file ) - 1 );
 $content  = trim($file[$arr]);
 
-//格式化判断，输出js或纯文本
+if (isset($_GET['charset']) && !empty($_GET['charset'])) {
+    $charset = $_GET['charset'];
+    if (strcasecmp($charset,"gbk") == 0 ) {
+        $content = mb_convert_encoding($content,'gbk', 'utf-8');
+    }
+} else {
+    $charset = 'utf-8';
+}
+
 if ($_GET['encode'] === 'js') {
     echo "function hitokoto(){document.write('" . $content ."');}";
 } else {
