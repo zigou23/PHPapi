4726c364a5a3a23d10812c98aae9eeee6e90e4d8
zigou <40587032+zigou23@users.noreply.github.com>
2021-10-31 00:57:15 +0800

    Update index.php

--- img/bing/daily/index.php
+++ img/bing/daily/index.php
@@ -1,5 +1,5 @@
 <?php
-$str = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+$str = file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 $str = json_decode($str,true);
 $imgurl = 'https://cn.bing.com'.$str['images'][0]['url'];
 header("Location: {$imgurl}");
