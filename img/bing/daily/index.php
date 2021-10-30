2aa2cce9a22f9641ff9d09fb140113942a4f644d
zigou <40587032+zigou23@users.noreply.github.com>
2021-10-31 00:56:21 +0800

    Create index.php

---
+++ img/bing/daily/index.php
@@ -0,0 +1,6 @@
+<?php
+$str = file_get_contents('https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+$str = json_decode($str,true);
+$imgurl = 'https://cn.bing.com'.$str['images'][0]['url'];
+header("Location: {$imgurl}");
+?>
