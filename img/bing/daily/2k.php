c20a19a27eeacfab71f7c555ff23252d0b9e002e
zigou <40587032+zigou23@users.noreply.github.com>
2022-02-10 15:20:35 +0800

    Update 2k.php

--- img/bing/daily/2k.php
+++ img/bing/daily/2k.php
@@ -1,7 +1,7 @@
 <?php
     $str=file_get_contents('https://www.bing.com/HPImageArchive.aspx?idx=0&n=1');
-if (preg_match("/<urlBase>(.+?)<\/urlBase>/ies", $str, $matches)) {
-    $imgurl='https://www.bing.com'.$matches[1].'_1920x1080.jpg';
+if (preg_match("/<url>(.+?)<\/url>/ies", $str, $matches)) {
+    $imgurl='https://www.bing.com'.$matches[1];
 }
 if ($imgurl) {
     header('Content-Type: image/JPEG');
