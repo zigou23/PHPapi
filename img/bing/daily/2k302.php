8cca22f66918fb72812fa130c04cf5a6324a14b1
zigou <40587032+zigou23@users.noreply.github.com>
2022-02-10 15:07:51 +0800

    Update 2k302.php

--- img/bing/daily/2k302.php
+++ img/bing/daily/2k302.php
@@ -1,7 +1,7 @@
 <?php
     $str=file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
 if (preg_match("/<urlBase>(.+?)<\/urlBase>/ies", $str, $matches)) {
-    $imgurl='https://cn.bing.com'.$matches[1].'_1920x1080.jpg';
+    $imgurl='https://cn.bing.com'.$matches[0].'_1920x1080.jpg';
 }
 if ($imgurl) {
     header("Location: $imgurl");    //输出302跳转
