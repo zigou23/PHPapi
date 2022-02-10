5eda22628d680b88f9239e2c4b5dd60dcdff9945
zigou <40587032+zigou23@users.noreply.github.com>
2022-02-10 17:28:24 +0800

    Update 2k.php

--- img/bing/daily/2k.php
+++ img/bing/daily/2k.php
@@ -1,8 +1,7 @@
 <?php
-    $str=file_get_contents('https://www.bing.com/HPImageArchive.aspx?idx=0&n=1');
-if (preg_match("/<url>(.+?)<\/url>/ies", $str, $matches)) {
-    $imgurl='https://www.bing.com'.$matches[1];
-}
+$str = file_get_contents('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+$str = json_decode($str,true);
+$imgurl = 'https://www.bing.com'.$str['images'][0]['url'];
 if ($imgurl) {
     header('Content-Type: image/JPEG');
     @ob_end_clean();
